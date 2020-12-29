816377df799cfba246d83de21ede2070abe5e4cc
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-12-29 08:43:28 +0000

    Linting.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,20 +10,16 @@ from rsc_on_this_day.__main__ import main
 def test_cli(file_regression: FileRegressionFixture):
 
 	runner = CliRunner()
+
 	result: Result = runner.invoke(main, catch_exceptions=False, args=["10", "13"])
-
 	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
 
-	runner = CliRunner()
-	result: Result = runner.invoke(main, catch_exceptions=False, args=["Oct", "13"])
-
+	result = runner.invoke(main, catch_exceptions=False, args=["Oct", "13"])
 	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
 
-	runner = CliRunner()
-	result: Result = runner.invoke(main, catch_exceptions=False, args=["October", "13"])
-
+	result = runner.invoke(main, catch_exceptions=False, args=["October", "13"])
 	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
 
@@ -31,7 +27,7 @@ def test_cli(file_regression: FileRegressionFixture):
 def test_cli_clear_cache(file_regression: FileRegressionFixture):
 
 	runner = CliRunner()
-	result: Result = runner.invoke(main, catch_exceptions=False, args=["--clear-cache"])
 
+	result: Result = runner.invoke(main, catch_exceptions=False, args=["--clear-cache"])
 	assert result.exit_code == 0
 	check_file_regression(result.stdout.rstrip(), file_regression)
