de5f3091e8b518e5a3c999f35193b1adae5ba9c0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-28 11:38:44 +0000

    Make use of new features in consolekit.testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,16 +12,16 @@ def test_cli(file_regression: FileRegressionFixture):
 	runner = CliRunner()
 
 	result: Result = runner.invoke(main, catch_exceptions=False, args=["10", "13"])
-	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
+	result.check_stdout(file_regression)
 
 	result = runner.invoke(main, catch_exceptions=False, args=["Oct", "13"])
-	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
+	result.check_stdout(file_regression)
 
 	result = runner.invoke(main, catch_exceptions=False, args=["October", "13"])
-	check_file_regression(result.stdout.rstrip(), file_regression)
 	assert result.exit_code == 0
+	result.check_stdout(file_regression)
 
 
 def test_cli_clear_cache(file_regression: FileRegressionFixture):
@@ -30,4 +30,4 @@ def test_cli_clear_cache(file_regression: FileRegressionFixture):
 
 	result: Result = runner.invoke(main, catch_exceptions=False, args=["--clear-cache"])
 	assert result.exit_code == 0
-	check_file_regression(result.stdout.rstrip(), file_regression)
+	result.check_stdout(file_regression)
